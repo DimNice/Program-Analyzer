389fd5859577ee54431e6cbbc4187abc825450a1
Демир Джабраилов <dimadr66@gmail.com>
2024-05-30 23:31:34 +0400

    add .exe file

--- test_generator.py
+++ test_generator.py
@@ -14,11 +14,11 @@ class TestGenerator:
         if vuln_codes is None:
             all_vuln_codes = test_lib.vuln_dict.keys()
             vuln_codes = random.sample(all_vuln_codes, random.randint(3, 12))
-        
+            print(vuln_codes)
         # take one fragment for each vuln + some number of correct fragments
         fragments = [random.choice(test_lib.vuln_tests[vc]) for vc in vuln_codes]
         fragments += random.sample(test_lib.correct_fragments, random.randint(3, 10))
-        
+
         # populate blocks with their fragments
         for f in fragments:
             # wrap simple fragments that consist of only one block
